6e8eafff09c7fa87f5b5d3a255b68da8215fde3e
Jonni Pitkänen <jonni.pitkanen@wippies.fi>
2017-01-30 19:55:53 +0200

    Add Champion class Add view controller code

---
+++ src/application/Champion.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.ArrayList;
+
+enum Position {
+	Top,
+	Mid,
+	Jungle,
+	Adc,
+	Support
+}
+
+public class Champion {
+
+	public static String name;
+	public static String title;
+	public static ArrayList<String> roles;
+	public Position pos;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
--- src/application/ViewController.java
+++ src/application/ViewController.java
@@ -1,14 +1,37 @@
 package application;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
 public class ViewController {
-	@FXML
-    private Button AddChampionButton;
+
+	public static ObservableList<String> ChampionText = FXCollections.observableArrayList();
+	public static ObservableList<Object> Data = FXCollections.observableArrayList();
+	
+    @FXML
+    private ListView<String> ChampionsList;
 
     @FXML
-    void AddChampionClicked() {
-    	AddChampionButton.setText("Ayy lmao");
+    private Button AddChampionButton;
+	
+	@FXML
+    public void initialize() {
+    	ChampionText.addAll("Test1", "Test2", "Test3");
+    	Champion champ = new Champion();
+    	champ.pos = Position.Adc;
+    }
+
+    @FXML
+    void AddChampionClicked(MouseEvent event) {
+    	
+    }
+
+    @FXML
+    void ChampionSelected(MouseEvent event) {
+
     }
 }
