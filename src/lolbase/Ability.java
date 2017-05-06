b4594b9401cad95e8e782135dcf7273d71a11b30
Jonni Pitkänen <jonni.pitkanen@wippies.fi>
2017-05-06 21:31:44 +0300

    add SQL stuff for abilities and skins

--- src/lolbase/Ability.java
+++ src/lolbase/Ability.java
@@ -1,9 +1,19 @@
 package lolbase;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Class object for abilities
  */
 public class Ability {
+	
+	/**
+	 * id
+	 */
+	public int id;
 	/**
 	 * ability's name
 	 */
@@ -35,4 +45,47 @@ public class Ability {
 		this.ChampionID = -1;
 		this.imageURL = "";
 	}
+	
+	public String createAbilitiesDataBase() {
+		return "CREATE TABLE Abilities (" +
+				"id INTEGER PRIMARY KEY AUTOINCREMENT , " +
+				"name VARCHAR(100) NOT NULL , " +
+				"school VARCHAR(100) NOT NULL , " +
+				"ChampionID INTEGER NOT NULL, " +
+				"imageURL VARCHAR(200) NOT NULL, " +
+				")";
+	}
+	
+	public PreparedStatement formatCreate(Connection con) throws SQLException {
+		PreparedStatement sql = con.prepareStatement("INSERT INTO Abilities" +
+				"(id, name, description, school, ChampionID, imageURL, " +
+				"VALUES (?, ?, ?, ?, ?, ?)");
+		
+		if(id != 0) sql.setInt(1,  id);
+		else sql.setString(1,  null);
+		sql.setString(2, name);
+		sql.setString(3, description);
+		sql.setString(4, school );
+		sql.setInt(5, ChampionID );
+		sql.setString(6, imageURL);
+		
+		return sql;
+	}
+	
+	public void tarkistaID(ResultSet rs) throws SQLException {
+		if(!rs.next() ) return;
+		int id = rs.getInt(1);
+		if (id == this.id) return;
+		this.id = id;
+	}
+	
+	public void parse(ResultSet results) throws SQLException {
+		this.id = results.getInt("id");
+		this.name = results.getString("name");
+		this.description = results.getString("description");
+		this.school = results.getString("school");
+		this.ChampionID = results.getInt("ChampionID");
+		this.imageURL = results.getString("imageURL");
+	}
+	
 }
--- src/lolbase/Skin.java
+++ src/lolbase/Skin.java
@@ -1,9 +1,19 @@
 package lolbase;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Class object for skins
  */
 public class Skin {
+	
+	/**
+	 * id
+	 */
+	public int id;
 	/**
 	 * skin's name
 	 */
@@ -22,5 +32,43 @@ public class Skin {
 	 */
 	public Skin(){
 		this.champID = -1;
+	}
+	
+	public String createSkinsDataBase() {
+		return "CREATE TABLE Skins (" +
+				"id INTEGER PRIMARY KEY AUTOINCREMENT , " +
+				"name VARCHAR(100) NOT NULL , " +
+				"champID INTEGER NOT NULL , " +
+				"imgURL VARCHAR(200) NOT NULL, " +
+				")";
+	}
+	
+	public PreparedStatement formatCreate(Connection con) throws SQLException {
+		PreparedStatement sql = con.prepareStatement("INSERT INTO Skins" +
+				"(id, name, champID, imgURL, " +
+				"VALUES (?, ?, ?, ?)");
+		
+		if(id != 0) sql.setInt(1,  id);
+		else sql.setString(1,  null);
+		sql.setString(2, name);
+		sql.setInt(3, champID);
+		sql.setString(4, imgURL );
+		
+		return sql;
+	}
+	
+	public void tarkistaID(ResultSet rs) throws SQLException {
+		if(!rs.next() ) return;
+		int id = rs.getInt(1);
+		if (id == this.id) return;
+		this.id = id;
+	}
+	
+	public void parse(ResultSet results) throws SQLException {
+		this.id = results.getInt("id");
+		this.name = results.getString("name");
+		this.champID = results.getInt("champID");
+		this.imgURL = results.getString("imgURL");
+
 	}
 }
